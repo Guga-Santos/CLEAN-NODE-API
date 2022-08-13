f331af3d0fda6babd96fc1a2744904a623b5d750
GugaSantos <guga.santos.guga@gmail.com>
2022-08-13 19:18:09 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -2,4 +2,3 @@ export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
 export * from '../../protocols/add-account-repository'
 export * from '../../protocols/encrypter'
-
