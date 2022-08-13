f85ff05425a024751accfceda193a2ea9f0d7bc5
GugaSantos <guga.santos.guga@gmail.com>
2022-08-13 17:22:37 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,5 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
+export * from '../../protocols/add-account-repository'
 export * from '../../protocols/encrypter'
+
