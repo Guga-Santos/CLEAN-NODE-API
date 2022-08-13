19a3b5139be0fa710e0142bbace198bd3304b339
GugaSantos <guga.santos.guga@gmail.com>
2022-08-12 22:26:20 -0300

    refactor: change import

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -2,4 +2,3 @@ export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
-
